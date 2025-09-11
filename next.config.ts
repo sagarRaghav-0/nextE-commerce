b08553b87ffb96401731e8b418850967ccf39bb2
raghav-000 <rajpr071220@gamil.com>
2025-09-11 20:31:24 +0530

    fix: enable PWA only in production

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   images: {
     domains: ["uskmnrcplvfskbrduxzh.supabase.co"],
-    
   },
-  reactStrictMode:false,
-  devIndicators:false
+  reactStrictMode: false,
+  devIndicators: false,
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public", // service worker + manifest will be stored here
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // 👈 disable PWA in dev
+})(nextConfig);
